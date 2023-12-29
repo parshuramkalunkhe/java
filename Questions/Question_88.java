5d86d144006c7ffdbbfbe7e90520a6e072727969
parshuramkalunkhe <parshuramkalunkhe@gmail.com>
2023-12-29 09:10:00 +0530

    29/12/2023 - Questions

---
+++ Questions/Question_88.java
@@ -0,0 +1,44 @@
+// Q.88
+
+import java.util.Scanner;
+
+public class Question_88 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter size of array : ");
+		int size = sc.nextInt();
+		
+		System.out.print("Enter k'th number : ");
+		int k = sc.nextInt();
+		
+		System.out.println("Enter array elements.");
+		int[] arr = new int[size];
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 1; i < k; i++) {
+			int maxEle = 0;
+			for (int m = 0; m < arr.length; m++) {
+				maxEle = Math.max(maxEle, arr[m]);
+			}
+			for (int j = 0; j < arr.length; j++) {
+				arr[j] = maxEle - arr[j];
+			}
+		}
+		
+		for (int i = 0, j = arr.length - 1; i < arr.length; i++, j--) {
+			arr[i] = arr[i] + arr[j];
+			arr[j] = arr[i] - arr[j];
+			arr[i] = arr[i] - arr[j];
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		sc.close();
+	}
+}
---
+++ Questions/Question_90.java
@@ -0,0 +1,46 @@
+//Q.90.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Question_90 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter size : ");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Enter elements.");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("Enter k'th number : ");
+		int k = sc.nextInt();
+		int[] arr1 = new int[k];
+		int[] arr2 = new int[size - k];
+		for (int i = 0; i < k; i++) {
+			arr1[i] = arr[i];
+		}
+		for (int i = k; i < size; i++) {
+			arr2[i - k] = arr[i];
+		}
+
+		Arrays.sort(arr1, 0, k);
+		Arrays.sort(arr2, 0, size - k);
+
+		int len2 = size - k;
+		for (int i = 0; i < size; i++) {
+			if (i < k) {
+				arr[i] = arr1[i];
+			} else {
+				arr[i] = arr2[len2 - 1];
+				len2--;
+			}
+		}
+		for (int i = 0; i < size; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		
+		sc.close();
+	}
+}
